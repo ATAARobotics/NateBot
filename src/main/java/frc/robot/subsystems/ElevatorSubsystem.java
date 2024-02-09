f880b844dd765c3f8a40fbac3298fc12551e90ee
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-02-08 19:29:46 -0700

    Update ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,56 +1,93 @@
+// Encoder Distance Between Top & Bottom: 15205
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.*;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalInput;
 
-public class ElevatorSubsystem {
+public class ElevatorSubsystem extends SubsystemBase {
     public static Joystick xBoxController = new Joystick(0);
 
-    public static DigitalInput bottomStop = new DigitalInput(0);
+    // public static DigitalInput bottomStop = new DigitalInput(0);
     public static DigitalInput topStop = new DigitalInput(1);
 
     private static final TalonSRX Elevator_Motor_1 = new TalonSRX(Constants.Elevator_1);
     private static final TalonSRX Elevator_Motor_2 = new TalonSRX(Constants.Elevator_2);
 
-    public static void elevatorInit() {
+    public Counter Encoder;
+
+    public double RightTrigger;
+    public double LeftTrigger;
+
+    public static boolean tankDrive = false;
+    public static int tankDriveInvert = 1;
+
+    public double trigger;
+    public double ElevatorDistance = 0;
+
+    public ElevatorSubsystem() {
+        Encoder = new Counter(0);
+        Encoder.setDistancePerPulse(100);
         Elevator_Motor_2.follow(Elevator_Motor_1);
 
         Elevator_Motor_1.setInverted(false);
         Elevator_Motor_2.setInverted(InvertType.OpposeMaster);
+
+        Elevator_Motor_1.set(ControlMode.PercentOutput, -1);
     }
 
-    public static void elevatorPeriodic() {
-        boolean bottomStop_Boolean = bottomStop.get();
-        boolean topStop_Boolean = topStop.get();
+    public void periodic() {
 
-        double xBoxLeftTrigger = -xBoxController.getRawAxis(2);
-        double xBoxRightTrigger = xBoxController.getRawAxis(3);
+        // boolean bottomStop_Boolean = bottomStop.get();
+        // boolean topStop_Boolean = topStop.get();
+        LeftTrigger = xBoxController.getRawAxis(2);
+        RightTrigger = xBoxController.getRawAxis(3);
+        trigger = RightTrigger - LeftTrigger;
 
-        double xBoxTrigger = 0.0;
-
-        if (Constants.Deadzone_Factor <= Math.abs(xBoxRightTrigger - 0)) {
-            if (bottomStop_Boolean) {
-                xBoxTrigger = 0.0;
-            }
-            else {
-                xBoxTrigger = xBoxRightTrigger;
-            }
+        if (Math.abs(trigger) >= Constants.Deadzone_Factor) {
+            Elevator_Motor_1.set(ControlMode.PercentOutput, -trigger);
+        } else {
+            Elevator_Motor_1.set(ControlMode.PercentOutput, 0);
         }
 
-        if (Constants.Deadzone_Factor <= Math.abs(xBoxLeftTrigger - 0)) {
-            if (topStop_Boolean) {
-                xBoxTrigger = 0.0;
-            }
-            else {
-                xBoxTrigger = xBoxLeftTrigger;
-            }
+        // Elevator Distance Up & Down
+
+        String ElevatorDirection;
+
+        if (trigger >= Constants.Deadzone_Factor) {
+            ElevatorDirection = "Up";
+        } else if (trigger <= -Constants.Deadzone_Factor) {
+            ElevatorDirection = "Down";
+        } else {
+            ElevatorDirection = "Stopped";
         }
-        
-        Elevator_Motor_1.set(ControlMode.PercentOutput, xBoxTrigger);
+
+        SmartDashboard.putString("ElevatorDirection", ElevatorDirection);
+        SmartDashboard.putBoolean("ElevatorStopped", Encoder.getStopped());
+        SmartDashboard.putNumber("Encoder", Encoder.get());
     }
+
+    public CommandBase rightBumper() {
+        return run(
+                () -> {
+                    Elevator_Motor_1.set(ControlMode.PercentOutput, 1);
+                });
+    }
+
+    public CommandBase leftBumper() {
+        return run(
+                () -> {
+                    Elevator_Motor_1.set(ControlMode.PercentOutput, -1);
+                });
+    }
+
 }
