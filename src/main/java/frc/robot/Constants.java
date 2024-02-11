e574eba487030abe325c0bfdbd42a753dc7d36b9
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-02-10 17:27:49 -0700

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
   public static final int Elevator_1 = 5;
   public static final int Elevator_2 = 6;
 
-  public static final double Deadzone_Factor = 0.2;
+  public static final double Deadzone_Factor = 0.1;
 
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
